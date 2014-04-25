72e31d462845aa3b2fe3f3a3bb6a057adff44aaf
ddewaele <ddewaele@gmail.com>
2014-04-25 22:46:45 +0200

    pom cleanup and constants update

--- src/com/ecs/sample/Constants.java
+++ src/com/ecs/sample/Constants.java
@@ -4,8 +4,8 @@ public class Constants {
 
 	public static final String TAG = "AndroidTwitterOauth1";
 	
-	public static final String API_KEY = "lKWXw71tfN6UxYSqsFQvJ6vem";
-	public static final String API_SECRET= "uVvC53uDY4cjuBrt2HfeI1nMCXAnjFWtQnJZ1aj0ZKFtEdnqtA";
+	public static final String API_KEY = "PUT YOUR TWITTER API KEY HERE";
+	public static final String API_SECRET= "PUT YOUR TWITTER API SECRET HERE";
 	
 	public static final String REQUEST_URL = "https://api.twitter.com/oauth/request_token";
 	public static final String ACCESS_URL = "https://api.twitter.com/oauth/access_token";
